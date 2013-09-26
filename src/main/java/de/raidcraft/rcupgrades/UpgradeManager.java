647c2af1b646dafbd2136073997490939b5fa167
Philip Urban <urban.philip@googlemail.com>
2013-09-26 18:40:27 +0200

    added delete method

--- src/main/java/de/raidcraft/rcupgrades/UpgradeManager.java
+++ src/main/java/de/raidcraft/rcupgrades/UpgradeManager.java
@@ -1,6 +1,8 @@
 package de.raidcraft.rcupgrades;
 
+import de.raidcraft.RaidCraft;
 import de.raidcraft.rcupgrades.api.holder.UpgradeHolder;
+import de.raidcraft.rcupgrades.tables.TUpgradeHolder;
 import org.bukkit.configuration.ConfigurationSection;
 
 /**
@@ -27,5 +29,11 @@ public class UpgradeManager {
         UpgradeHolder<O> upgradeHolder = new DatabaseUpgradeHolder<O>(object, holderConfig);
         upgradeHolder.save();
         return upgradeHolder;
+    }
+
+    public void deleteUpgradeHolder(int id) {
+
+        TUpgradeHolder tUpgradeHolder = RaidCraft.getDatabase(RCUpgradesPlugin.class).find(TUpgradeHolder.class, id);
+        RaidCraft.getDatabase(RCUpgradesPlugin.class).delete(tUpgradeHolder);
     }
 }
